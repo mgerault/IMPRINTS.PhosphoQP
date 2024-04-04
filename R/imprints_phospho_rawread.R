72dc972177a0b77af5fed336e81a658b3cab9cc7
mgerault <67689583+mgerault@users.noreply.github.com>
2024-04-04 10:37:18 +0200

    Add files via upload
    Removed checking names in read.xlsx and read.csv functions

--- R/imprints_phospho_rawread.R
+++ R/imprints_phospho_rawread.R
@@ -1,45 +1,45 @@
 #' imprints_phospho_rawread
-#' 
+#'
 #' Function to read the phosphoproteomics data from Proteome Discoverer
-#' 
-#' @param data Your input data. It can the path to the output file from PD or a 
+#'
+#' @param data Your input data. It can the path to the output file from PD or a
 #'    data.frame corresponding to that output.
-#' @param treatment The treatment corresponding to each fo your TMT channel. It should be 
+#' @param treatment The treatment corresponding to each fo your TMT channel. It should be
 #'   the same length as your number of TMT channel and should follow the format 'Rep_treatment'
 #'   like 'B1_Vehicle' for example.
 #' @param contanimant Character to tell which suffix indicating a protein is a contaminant.
-#' 
+#'
 #' @return Your data filtered and in the right format to use the IMPRINTS.CETSA package.
 #'
 #' @export
 
 imprints_phospho_rawread <- function(data, treatment, contaminant = "Cont"){
   stopifnot(!any(duplicated(treatment)))
-  
+
   if(inherits(data, "character")){
     stopifnot(file.exists(data))
-    
+
     if(grepl("\\.txt$", data)){
       data <- readr::read_tsv(data)
     }
     else if(grepl("\\.xlsx$", data)){
-      data <- openxlsx::read.xlsx(data)
+      data <- openxlsx::read.xlsx(data, check.names = FALSE, sep.names = " ")
     }
     else if(grepl("\\.csv$", data)){
-      data <- readr::read_csv(data)
+      data <- readr::read_csv(data, check.names = FALSE)
     }
   }
   else if(!inherits(data, "data.frame")){
     stop("Your input can only be a file or a dataframe !")
   }
-  
+
   cn_needed <- c("^Sequence$", "^Modifications$", "^Modifications in Master Proteins$",
                  "^Positions in Master Proteins$", "^Master Protein Descriptions$",
                  "PSMs$")
   cn <- colnames(data)
   check_cn_needed <- sapply(cn_needed,
                             function(x) length(grep(x, cn)) == 1)
-  
+
   if(!(all(check_cn_needed))){
     cn_missing <- cn_needed[!check_cn_needed]
     stop(paste("The", ifelse(length(cn_missing) > 1, "columns", "column"),
@@ -50,7 +50,7 @@ imprints_phospho_rawread <- function(data, treatment, contaminant = "Cont"){
   else if(length(grep("Abundance:|Abundance F\\d{1,}", cn)) != length(treatment)){
     stop("Your number of raw abundance columns doesn't match your number of treatments !")
   }
-  
+
   # remove potential contaminants
   contaminants_torm <- grep("Bos taurus", data[["Master Protein Descriptions"]])
   contaminants_torm <- c(contaminants_torm, grep(paste0("^", contaminant), data[["Positions in Master Proteins"]]))
@@ -62,37 +62,37 @@ imprints_phospho_rawread <- function(data, treatment, contaminant = "Cont"){
     message(paste(length(contaminants_torm), "contaminants have been removed"))
     data <- data[-contaminants_torm,]
   }
-  
+
   # select columns
   data <- data[,grep("^Sequence$|^Modifications$|^(Modifications|Positions) in Master Proteins$|^Master Protein Descriptions$|PSMs$|(Abundance:|Abundance F\\d{1,})",
                      colnames(data)
   )
   ]
-  
+
   ## renaming columns
   colnames(data)[grep("PSMs$", colnames(data))] <- "sumPSMs"
-  
+
   colnames(data)[grep("^Abundance:|Abundance F\\d{1,}", colnames(data))] <- treatment
-  
+
   colnames(data) <- gsub(" ", ".", colnames(data))
-  
+
   # removing Mix and/or Empty channels
   mix_empty <- grep("^Mix|^Empty", colnames(data))
   if(length(mix_empty)){
     data <- data[,-mix_empty]
   }
-  
+
   ## filtering
   # only keep pep with quantitative info
-  data <- data[apply(data[7:ncol(data)], 1, function(x) sum(is.na(x)) < length(treatment) - 1),] 
-  
+  data <- data[apply(data[7:ncol(data)], 1, function(x) sum(is.na(x)) < length(treatment) - 1),]
+
   # only keep phospho peptides
   data <- data[grep("Phospho", data$Modifications.in.Master.Proteins),]
-  
-  
+
+
   # remove TMT modification information from modifications
   data$Modifications <- unlist(
-    lapply(strsplit(data$Modifications, "\\]; "), 
+    lapply(strsplit(data$Modifications, "\\]; "),
            function(x){
              x <- x[-grep("TMTpro", x)]
              if(length(grep("\\]$", x, invert = TRUE))){
@@ -102,19 +102,19 @@ imprints_phospho_rawread <- function(data, treatment, contaminant = "Cont"){
              x
            })
   )
-  
+
   data$Modifications.in.Master.Proteins <- NULL
-  
-  
+
+
   # put in right format in order to use existing package IMPRINTS.CETSA
   colnames(data)[c(1,2,4,5)] <- c("id", "sumUniPeps", "description", "countNum")
   data <- data[,c(1,4, 6:ncol(data), 2,3,5)]
   colnames(data)[3:(ncol(data)-3)] <- paste0("25C_", colnames(data)[3:(ncol(data)-3)])
-  
+
   # make sure every id is different
   data$id <- paste0(data$id, "_", 1:nrow(data))
   # need to order according id for imprints_normalization function (expression used)
-  data <- data[order(data$id),] 
-  
+  data <- data[order(data$id),]
+
   return(data)
 }
